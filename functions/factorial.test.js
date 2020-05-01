be7ae5245864575ed66b1f95042a086245e94881
Robert Pym <rob_pym@hotmail.com>
2020-05-01 16:42:37 +0100

    Failing test for factorial negative value handling

--- functions/factorial.test.js
+++ functions/factorial.test.js
@@ -16,4 +16,16 @@ describe('factorial test cases', () => {
   test('5! is 5x4x3x2x1', () => {
     expect(factorial(5)).toBe(5 * 4 * 3 * 2 * 1);
   });
+
+  test('10! is 10x9x8x7x6x5x4x3x2x1', () => {
+    expect(factorial(10)).toBe(10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1);
+  });
+
+  test('Expected handling of negatives', () => {
+    try {
+      factorial(-1);
+    } catch (err) {
+      expect(typeof err).toBe('RangeError');
+    }
+  });
 });
