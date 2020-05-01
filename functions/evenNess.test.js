99423567204db47b63af14bf30c244d5fc0c3875
Robert Pym <rob_pym@hotmail.com>
2020-05-01 16:08:59 +0100

    Separate fixtures for isOdd and isEven

--- functions/evenNess.test.js
+++ functions/evenNess.test.js
@@ -1,17 +1,21 @@
 const { isEven, isOdd } = require('./evenNess.js');
 
-test('1 is not even', () => {
-  expect(isEven(1)).not.toBe(true);
+describe('iEven test cases', () => {
+  test('1 is not even', () => {
+    expect(isEven(1)).not.toBe(true);
+  });
+
+  test('2 is even', () => {
+    expect(isEven(2)).toBe(true);
+  });
 });
 
-test('2 is even', () => {
-  expect(isEven(2)).toBe(true);
-});
+describe('isOdd test cases', () => {
+  test('1 is odd', () => {
+    expect(isOdd(1)).toBe(true);
+  });
 
-test('1 is odd', () => {
-  expect(isOdd(1)).toBe(true);
-});
-
-test('2 is not odd', () => {
-  expect(isOdd(2)).not.toBe(true);
+  test('2 is not odd', () => {
+    expect(isOdd(2)).not.toBe(true);
+  });
 });
